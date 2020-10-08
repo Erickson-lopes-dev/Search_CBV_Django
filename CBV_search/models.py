bc68bb1de57bc38f38f377b9d972626fea39797b
Erickson Lopes <erickson-lopes-dev>
2020-10-08 11:42:18 -0300

    created models.py

--- CBV_search/models.py
+++ CBV_search/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseRegister(models.Model):
+    created_date = models.DateField('Data da Criação', auto_now=True)
+    created_hours = models.TimeField('Horario de criação', auto_now=True)
+
+    modified_date = models.DateField('Data de modificação', auto_now_add=True)
+    modified_hours = models.TimeField('Horario de modificação', auto_now_add=True)
+
+    active = models.BooleanField('Ativado ?', default=True)
+
+
+class Person(BaseRegister):
+    name = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail')
+
+    birth_time = models.TimeField('Horario de nascimento', auto_now=False, auto_now_add=False)
+    date_birth = models.DateField('Data de nascimento')
